30f83308599f3a0f65707149230deb677822a246
yohan stoeckle <you@example.com>
2021-02-22 19:20:06 +0100

    create+update

--- front/src/views/Create.vue
+++ front/src/views/Create.vue
@@ -5,25 +5,25 @@
       <b-button variant="outline-danger">New</b-button>
     </div>
     
-    <h1>New picture</h1>
+    <h1>Update</h1>
 
     <div id= "Forms">
       <b-form id="bform">
         <div>
           <div>
             <p>Nom de la photo</p>
-            <b-form-input v-model="text" placeholder="Filename"></b-form-input>
+            <b-form-input v-model="name" placeholder="Filename"></b-form-input>
           </div>
 
           <div>
             <p>Type d'image</p>
-            <b-form-input v-model="text" placeholder="Image type"></b-form-input>
+            <b-form-input v-model="type" placeholder="Image type"></b-form-input>
           </div>
       
           <div>
             <b-form-group label="Photo avec produit" v-slot="{ ariaDescribedby }">
-              <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" name="some-radios" value="A">Oui</b-form-radio>
-              <b-form-radio v-model="selected" :aria-describedby="ariaDescribedby" name="some-radios" value="B">Non</b-form-radio>
+              <b-form-radio v-model="picture_product" :aria-describedby="ariaDescribedby" name="some-radios" value="oui">Oui</b-form-radio>
+              <b-form-radio v-model="picture_product" :aria-describedby="ariaDescribedby" name="some-radios" value="non">Non</b-form-radio>
             </b-form-group>
 
           </div>
@@ -31,15 +31,15 @@
         <div>
           <div>
             <b-form-group label="Photo avec Humain" v-slot="{ ariaDescribedby }">
-                <b-form-radio v-model="selected1" :aria-describedby="ariaDescribedby" name="some-radios1" value="A">Oui</b-form-radio>
-                <b-form-radio v-model="selected1" :aria-describedby="ariaDescribedby" name="some-radios1" value="B">Non</b-form-radio>
+                <b-form-radio v-model="picture_human" :aria-describedby="ariaDescribedby" name="some-radios1" value="oui">Oui</b-form-radio>
+                <b-form-radio v-model="picture_human" :aria-describedby="ariaDescribedby" name="some-radios1" value="non">Non</b-form-radio>
             </b-form-group>
           </div>
 
           <div>
             <b-form-group label="Photo institutionnelle" v-slot="{ ariaDescribedby }">
-              <b-form-radio v-model="selected2" :aria-describedby="ariaDescribedby" name="some-radios2" value="A">Oui</b-form-radio>
-              <b-form-radio v-model="selected2" :aria-describedby="ariaDescribedby" name="some-radios2" value="B">Non</b-form-radio>
+              <b-form-radio v-model="picture_institutional" :aria-describedby="ariaDescribedby" name="some-radios2" value="oui">Oui</b-form-radio>
+              <b-form-radio v-model="picture_institutional" :aria-describedby="ariaDescribedby" name="some-radios2" value="non">Non</b-form-radio>
             </b-form-group>
 
           </div>
@@ -47,20 +47,20 @@
         <div>
           <div>
             <p>Credits de la photo</p>
-            <b-form-input v-model="text" placeholder="Credits"></b-form-input>
+            <b-form-input v-model="credits" placeholder="Credits"></b-form-input>
           </div>
 
           <div>
-            <b-form-group label="Format" v-slot="{ ariaDescribedby }">
-              <b-form-radio v-model="selected4" :aria-describedby="ariaDescribedby" name="some-radios3" value="A">Oui</b-form-radio>
-              <b-form-radio v-model="selected4" :aria-describedby="ariaDescribedby" name="some-radios3" value="B">Non</b-form-radio>
+            <b-form-group label="format" v-slot="{ ariaDescribedby }">
+              <b-form-radio v-model="format" :aria-describedby="ariaDescribedby" name="some-radios3" value="vertical">vertical</b-form-radio>
+              <b-form-radio v-model="format" :aria-describedby="ariaDescribedby" name="some-radios3" value="horizontal">horizontal</b-form-radio>
             </b-form-group>
 
           </div>
 
           <div id= "tag">
             <label for="tags-basic">Type a new tag and press enter</label>
-            <b-form-tags input-id="tags-basic" v-model="value"></b-form-tags>
+            <b-form-tags input-id="tags-basic" v-model="tags"></b-form-tags>
             <p class="mt-2">Value: {{ value }}</p>
           </div>
 
